9473c8a29e55d2f93ed9cc60778b0e057b5170b8
Topias Mariapori <topias@mariapori.fi>
2022-08-21 21:59:32 +0300

    Modelchanges

---
+++ KuittiApp/Data/Migrations/20220821173012_modelchanges.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KuittiApp.Data.Migrations
+{
+    public partial class modelchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PVM",
+                table: "Kuitit",
+                newName: "OstoPVM");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TakuuKK",
+                table: "Kuitit",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TakuuKK",
+                table: "Kuitit");
+
+            migrationBuilder.RenameColumn(
+                name: "OstoPVM",
+                table: "Kuitit",
+                newName: "PVM");
+        }
+    }
+}
